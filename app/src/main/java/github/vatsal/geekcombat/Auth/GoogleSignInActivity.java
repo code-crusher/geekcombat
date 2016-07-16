f80a00d0e437d6aa371573a747e7404def846f03
Vatsal-Bajpai <vatsal.bajpai05@gmail.com>
2016-07-16 12:02:45 +0530

    added json

--- app/src/main/java/github/vatsal/geekcombat/Auth/GoogleSignInActivity.java
+++ app/src/main/java/github/vatsal/geekcombat/Auth/GoogleSignInActivity.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2016 Google Inc. All Rights Reserved.
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -82,7 +82,7 @@ public class GoogleSignInActivity extends BaseActivity implements
         // [START config_signin]
         // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken("431122295230-74bs0uj92f6amhtnpvnq2cncdvdmklr5.apps.googleusercontent.com")
                 .requestEmail()
                 .build();
         // [END config_signin]
@@ -146,6 +146,8 @@ public class GoogleSignInActivity extends BaseActivity implements
                 // Google Sign In was successful, authenticate with Firebase
                 GoogleSignInAccount account = result.getSignInAccount();
                 firebaseAuthWithGoogle(account);
+
+
             } else {
                 // Google Sign In failed, update UI appropriately
                 // [START_EXCLUDE]
