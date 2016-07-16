d95ac0c6f6d83d655167d087d15e9cc18d45aaf3
Vatsal-Bajpai <vatsal.bajpai05@gmail.com>
2016-07-16 11:11:51 +0530

    init

---
+++ app/src/main/java/github/vatsal/geekcombat/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package github.vatsal.geekcombat.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import github.vatsal.geekcombat.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
